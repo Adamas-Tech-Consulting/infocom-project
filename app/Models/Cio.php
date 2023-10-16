13f587924b75e6280681988fe5a8e44c47e3613d
Avik Basak <avik1.basak@adamastech.in>
2023-10-16 17:11:21 +0530

    CIO Update Issue

--- app/Models/Cio.php
+++ app/Models/Cio.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Cio extends Model
 {
     protected $table = 'cio';
-    protected $fillable = ['registration_request_id','designation','company_name','linkedin_url','image'];
+    protected $fillable = ['registration_request_id','type','designation','company_name','linkedin_url','image'];
 }
